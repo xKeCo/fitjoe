5048d8d572c90c7b3c8cac59129a79bdd2df0e24
Kevin Collazos <collazo-s@hotmail.com>
2022-03-26 17:57:16 -0400

    Test Web Hook Validation

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,13 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+const { verifyWebhookSignature } = require("@graphcms/utils");
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   console.log(req.body);
+  console.log(req.body.data.collections);
   console.log(req.headers);
+
+  const isValid = verifyWebhookSignature({
+    body: req.body,
+    signature: req.headers["gcms-signature"],
+    secret: process.env.MY_SECRET_TOKEN,
+  });
+
   // Check for secret to confirm this is a valid request
-  if (req.headers["gcms-signature"] !== process.env.MY_SECRET_TOKEN) {
+  if (isValid) {
     return res.status(401).json({ message: "Invalid token" });
   }
   try {
